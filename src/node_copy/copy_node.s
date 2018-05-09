b7a30e8c65694ce87062fdf5fb90f1689b5aaaa3
Erin van der Veen <erinvanderveen@outlook.com>
2018-05-09 11:43:16 +0200

    Volatile registers

--- src/node_copy/copy_node.s
+++ src/node_copy/copy_node.s
@@ -5,31 +5,35 @@
 
 .text
 
-# rax: B0
-# rbx: B1
-# rcx: A0
-# rdx: A1
-# rbp: N/A
-# rsi: A_p
-# rdi: Heap_p
-# rsp: B_p
-# r8:  A2
-# r9:  A3
-# r10: B2
-# r11: B3
-# r12: B4
-# r13: B4
-# r14: B5
-# r15: Number of free words on heap
+#       rax: B0
+# (res) rbx: B1
+#       rcx: A0
+#       rdx: A1
+# (res) rbp: N/A
+#       rsi: A_p
+#       rdi: Heap_p
+#       rsp: B_p
+#       r8:  A2
+#       r9:  A3
+#       r10: B2
+#       r11: B3
+# (res) r12: B4
+# (res) r13: B4
+# (res) r14: B5
+# (res) r15: Number of free words on heap
 
 __get__arity__asm:
-	call   create_space_a_stack
-
-	# To restore later
+	# Save all volatile registers
+	push   rax
 	push   rcx
+	push   rdx
 	push   rsi
 	push   rdi
 	push   rsp
+	push   r8
+	push   r9
+	push   r10
+	push   r11
 
 	# Set arguments
 	mov    rdi, rdi # Heap pointer
@@ -40,12 +44,17 @@ __get__arity__asm:
 	call   get_arity_c
 
 	# Restore registers
+	pop    r11
+	pop    r10
+	pop    r9
+	pop    r8
 	pop    rsp
 	pop    rdi
 	pop    rsi
+	pop    rdx
 	pop    rcx
+	pop    rax
 
-	# Return (rax will be the result of get_arity_c)
 	ret
 
 create_space_a_stack:
@@ -59,4 +68,9 @@ create_space_a_stack:
 	mov    rdx, rcx
 
 	# A0 is now free to be used
+	ret
+
+push_rax_a_stack:
+	call   create_space_a_stack
+	mov    rcx, rax
 	ret
