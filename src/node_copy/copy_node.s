44c44afb6453e2aa87047e28750e76ee1c4b8a03
Erin van der Veen <erinvanderveen@outlook.com>
2018-05-09 09:28:51 +0200

    Documentation + Create space a stack

--- src/node_copy/copy_node.s
+++ src/node_copy/copy_node.s
@@ -5,12 +5,26 @@
 
 .text
 
-# rcx: Top of A-Stack
-# rdi: Pointer to top of Heap
-# r15: Amount of free words on heap
-# rsi: ?
+# rax: B0
+# rbx: B1
+# rcx: A0
+# rdx: A1
+# rbp: N/A
+# rsi: A_p
+# rdi: Heap_p
+# rsp: B_p
+# r8:  A2
+# r9:  A3
+# r10: B2
+# r11: B3
+# r12: B4
+# r13: B4
+# r14: B5
+# r15: Number of free words on heap
 
 __get__arity__asm:
+	call   create_space_a_stack
+
 	# To restore later
 	push   rcx
 	push   rsi
@@ -32,4 +46,17 @@ __get__arity__asm:
 	pop    rcx
 
 	# Return (rax will be the result of get_arity_c)
+	ret
+
+create_space_a_stack:
+	# Put r9 on top of stack
+	mov    [rsi], r9
+	# Increase A heap
+	inc    rsi
+	# Shift A regs by 1
+	mov    r9, r8
+	mov    r8, rdx
+	mov    rdx, rcx
+
+	# A0 is now free to be used
 	ret
