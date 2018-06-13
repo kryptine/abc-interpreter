d3d9983855d7f285c1bac8963c1e962ad8278b3e
Camil Staps <info@camilstaps.nl>
2018-06-13 15:48:54 +0200

    Sort symbol table

--- src/bytecode_gen/bytecode_gen.c
+++ src/bytecode_gen/bytecode_gen.c
@@ -72,7 +72,6 @@ int main (int argc, char *argv[]) {
 	parse_files(input_files, nr_abc_files);
 
 	add_add_arg_labels();
-	make_undefined_labels_global();
 	write_program(output_file);
 
 	return 0;
