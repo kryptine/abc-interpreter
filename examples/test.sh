32616bfd118829a3622797ced85b24162a13fa5f
Camil Staps <info@camilstaps.nl>
2018-10-25 12:21:32 +0200

    Fix examples makefile

--- examples/test.sh
+++ examples/test.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 set -e
 
+make -C ../src\
+	graph_copy_with_names.dcl\
+	graph_copy_with_names.icl\
+	Clean\ System\ Files/interpret.a\
+	optimise\
+	bcgen\
+	link
+
 for dir in *; do
 	[ -d "$dir" ] || continue
 	echo -e "\033[0;33mTesting $dir\033[0m"
