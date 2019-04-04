52a9aa295ec4a298e7eb7843feac777d2d9b3fce
Camil Staps <info@camilstaps.nl>
2019-04-04 09:49:55 +0200

    Add `instruction 0` to the prelink preamble as a kind of eval_to_hnf_return

--- src/bcprelink.c
+++ src/bcprelink.c
@@ -19,7 +19,7 @@ enum section_type {
 #define _4chars2int(a,b,c,d)         ((uint64_t) (a+(b<<8)+(c<<16)+(d<<24)))
 #define _7chars2int(a,b,c,d,e,f,g)   ((uint64_t) (a+(b<<8)+(c<<16)+(d<<24)+((uint64_t)e<<32)+((uint64_t)f<<40)+((uint64_t)g<<48)))
 #define _8chars2int(a,b,c,d,e,f,g,h) ((uint64_t) (a+(b<<8)+(c<<16)+(d<<24)+((uint64_t)e<<32)+((uint64_t)f<<40)+((uint64_t)g<<48)+((uint64_t)h<<56)))
-uint64_t prelinker_preamble[658] = {
+uint64_t prelinker_preamble[660] = {
 	/*  0 */ 0, 0, 0, 7, _7chars2int('_','A','R','R','A','Y','_'),
 	/*  5 */ 0, 0, 0, 8, _8chars2int('_','S','T','R','I','N','G','_'),
 	/* 10 */ 0, 0, 0, 4, _4chars2int('B','O','O','L'),
@@ -62,6 +62,8 @@ uint64_t prelinker_preamble[658] = {
 		0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
 		0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0, 0,0,0,0,0,0,0,0,
 	/* 658 */
+		Cinstruction, 0, /* to return to JavaScript */
+	/* 660 */
 };
 
 void prepare_preamble(void) {
