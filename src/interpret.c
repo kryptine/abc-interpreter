kryptine/abc-interpreter

commit 6c776c0b77c57035339279de57b9ec4d5e675ca2

Remove unnecessary malloc in interpret()

src/interpret.c
@@ -443,13 +443,12 @@ int interpret(
 #endif
 
 	if (_pc != NULL) {
-		BC_WORD *ret=safe_malloc(sizeof(BC_WORD));
 #ifdef COMPUTED_GOTOS
-		*ret=(BC_WORD)&&eval_to_hnf_return;
+		BC_WORD ret=(BC_WORD)&&eval_to_hnf_return;
 #else
-		*ret=EVAL_TO_HNF_LABEL;
+		BC_WORD ret=EVAL_TO_HNF_LABEL;
 #endif
-		*++csp=(BC_WORD)ret;
+		*++csp=(BC_WORD)&ret;
 		pc=_pc;
 
 		if (0) {

commit cbac1b4e8d38e37ceb94f66da72daed9390ae6dc

Keep return label in memory during entire interpret() call (prevents segfault caused by 6c776c0b)

src/interpret.c
@@ -442,11 +442,12 @@ int interpret(
 	}
 #endif
 
+	BC_WORD ret;
 	if (_pc != NULL) {
 #ifdef COMPUTED_GOTOS
-		BC_WORD ret=(BC_WORD)&&eval_to_hnf_return;
+		ret=(BC_WORD)&&eval_to_hnf_return;
 #else
-		BC_WORD ret=EVAL_TO_HNF_LABEL;
+		ret=EVAL_TO_HNF_LABEL;
 #endif
 		*++csp=(BC_WORD)&ret;
 		pc=_pc;